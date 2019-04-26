5580437b0621b40019a4b8cd7439bbe26bfdac0a
MutaJosh <mutajoshua99@gmail.com>
2019-04-26 14:12:43 +0200

    Displaying on console

--- script.js
+++ script.js
@@ -2,5 +2,8 @@
 var myInp = document.getElementById('input1');
 
 function balanceScale(){
-	document.getElementById('results').innerHTML = myInp.value;
+	// document.getElementById('results').innerHTML = myInp.value;
+	// document.getElementById('results').style.backgroundColor = "teal";
+
+	console.log(myInp);
 }
