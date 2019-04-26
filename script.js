7c5e8d13fba8bcd71ffa46bd650f40ac4e4603e8
MutaJosh <mutajoshua99@gmail.com>
2019-04-26 19:22:33 +0200

    Adding the logo and other changes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
-var myInp = document.getElementById('input1');
-
+//var myInp = document.getElementById('input1');
+var a = 100;
 function balanceScale(){
-	// document.getElementById('results').innerHTML = myInp.value;
+	document.getElementById('results').innerHTML = a;
 	// document.getElementById('results').style.backgroundColor = "teal";
-
-	console.log(myInp);
 }
